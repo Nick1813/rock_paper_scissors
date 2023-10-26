5372069921334f131a84a0bcfd40bb5968365ea1
Nick1813 <142940269+Nick1813@users.noreply.github.com>
2023-10-26 14:42:34 -0700

    game complete

--- game.js
+++ game.js
@@ -1,67 +1,102 @@
-    let playerScore=0;
-    let computerScore=0;
+const game = ()=>{
+    let playerScore =0;
+    let computerScore =0;
+    let moves=0;
+const playGame =()=>{
+    const rockBtn =document.querySelector('.rock');
+    const scissorsBtn = document.querySelector('.scissors');
+    const paperBtn =document.querySelector('.paper');
+    const playerOption =[rockBtn,paperBtn,scissorsBtn];
+    const comOption =['rock','paper','scissors'];
 
-    
-function getComputerChoice(){
-    let comChoice=Math.random();
-    if(comChoice<.34){
-        return "rock";
-    } else if(comChoice <=.67){
-        return "paper";
-    } else {
-        return "scissors";
+    playerOption.forEach(option => {
+        option.addEventListener('click',function(){
+            const movesLeft = document.querySelector('.movesLeft');
+            moves++;
+            movesLeft.innerText ='Moves Left: '+(10-moves);
+
+            const choiceNumber = Math.floor(Math.random()*3);
+            const comChoice = comOption[choiceNumber];
+
+            winner(this.innerText,comChoice);
+
+            if(moves==10){
+                gameOver(playerOption,movesLeft);
+            }
+        })
+    })
+}
+
+const winner =(player,computer) =>{
+const result = document.querySelector('.result');
+const playerScoreBoard= document.querySelector('.p-count');
+const computerScoreBoard = document.querySelector('.c-count');
+player= player.toLowerCase();
+computer = computer.toLowerCase();
+if(player === computer){
+    result.textContent ="Tie";
+} else if(player=="rock"){
+    if(computer=='paper'){
+        result.textContent='You Lose';
+        computerScore++;
+        computerScoreBoard.textContent=computerScore;
+    } else{
+        result.textContent='You Win';
+        playerScore++;
+        playerScoreBoard.textContent=playerScore;
+    }
+} else if(player=='scissors'){
+    if(computer=='rock'){
+        result.textContent='You Lose';
+        computerScore++;
+        computerScoreBoard.textContent=computerScore;
+    } else{
+        result.textContent='You Win';
+        playerScore++;
+        playerScoreBoard.textContent=playerScore;
+    }
+} else if(player=='paper'){
+    if(computer=='scissors'){
+        result.textContent='You Lose';
+        computerScore++;
+        computerScoreBoard.textContent=computerScore;
+    } else{
+        result.textContent='You Win';
+        playerScore++;
+        playerScoreBoard.textContent=playerScore;
     }
 }
-
-
-
-function playRound(playerSelection, computerSelection){
-if(playerSelection === computerSelection){
-    return "Result is a tie";
-} else if(playerSelection==='rock' && computerSelection ==='paper'){
-    computerScore++;
-    return "You lose, paper beats rock";
-} else if(playerSelection==='rock' && computerSelection ==='scissors'){
-    playerScore++;
-    return "You win, rock beats scissors";
-} else if(playerSelection ==='paper' && computerSelection ==='rock'){
-    playerScore++;
-    return "You win, paper beats rock";
-} else if(playerSelection==='paper' && computerSelection ==='scissors'){
-    computerScore++;
-    return "You lose, scissors beats paper";
-} else if(playerSelection==='scissors' && computerSelection==='rock'){
-    computerScore++;
-    return "You lose, rock beats scissors";
-} else if(playerSelection==='scissors' && computerSelection ==='paper'){
-    playerScore++;
-    return "You win, scissors beats paper";
-}
 }
 
+const gameOver =(playerOption,movesLeft) =>{
+    const chooseMove=document.querySelector('.move');
+    const result=document.querySelector('.result');
+    const reload=document.querySelector('.reload');
 
-/*function game(){
-for(let i=0;i<5;i++){
-    let playerSelection = prompt("Enter rock, paper, or scissors");
-    var computerSelection=getComputerChoice();
-    playRound(playerSelection,computerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection,computerSelection));
-}
-gameOver();
-console.log(gameOver(playerScore,computerScore));
-}
-*/
-
-
-function gameOver(playerScore,computerScore){
+    playerOption.forEach(option => {
+        option.style.display ='none';
+    })
+    chooseMove.innerText='Game Over!';
+    movesLeft.style.display='none';
     if(playerScore>computerScore){
-    return "You win the game";
-} else if(computerScore>playerScore){
-    return "You lose the game";
-} else{
-    return "Game is a tie";
+        result.style.fontSize='2rem';
+        result.innerText='You Win';
+        result.style.color='green';
+    } else if(playerScore<computerScore){
+        result.style.fontSize='2rem';
+        result.innerText='You Lose';
+        result.style.color='red';
+    } else{
+        result.style.fontSize='2rem';
+        result.innerText='Tie';
+        result.style.color='black';
+    }
+    reload.innerText='Restart';
+    reload.style.display='flex';
+    reload.addEventListener('click',()=>{
+        window.location.reload();
+    })
 }
+playGame();
 }
 game();
-
